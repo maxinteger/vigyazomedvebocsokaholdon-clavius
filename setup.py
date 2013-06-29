111658ff93033be7eaff33ea990809d3b918afee
Vadasz Laszlo <maxinteger@gmail.com>
2013-06-29 18:48:20 +0200

    deps fix

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-		'Django==1.5.1'
-		'argparse==1.2.1'
-		'distribute==0.6.34'
-		'dj-database-url==0.2.1'
-		'django-debug-toolbar==0.9.4'
-		'djangorestframework==2.3.5'
-		'gunicorn==0.17.4'
+		'Django==1.5.1',
+		'argparse==1.2.1',
+		'distribute==0.6.34',
+		'dj-database-url==0.2.1',
+		'django-debug-toolbar==0.9.4',
+		'djangorestframework==2.3.5',
+		'gunicorn==0.17.4',
 		'wsgiref==0.1.2'
     ],
 )
