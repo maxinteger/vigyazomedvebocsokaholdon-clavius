488f446caffcaf0da8577cd54e539664adc03726
Vadasz Laszlo <maxinteger@gmail.com>
2013-06-29 18:37:42 +0200

    add deps

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='Vigyazo medvebocsok a holdon',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
+    author='Maxinteger',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django<=1.4'],
+    install_requires=[
+		'Django==1.5.1'
+		'argparse==1.2.1'
+		'distribute==0.6.34'
+		'dj-database-url==0.2.1'
+		'django-debug-toolbar==0.9.4'
+		'djangorestframework==2.3.5'
+		'gunicorn==0.17.4'
+		'wsgiref==0.1.2'
+    ],
 )
